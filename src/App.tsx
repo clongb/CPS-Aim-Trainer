6a59867f17120b6d3f684c877f11277cc154a786
unknown <clongb@udel.edu>
2021-09-24 15:08:08 -0400

    Fixed timer functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,31 @@ import React, { useState, useEffect } from 'react';
 
 function App(): JSX.Element {
   const [count, setCount] = useState(0);
-  const [timer, setTimer] = useState(0);
+  const [timer, setTimer] = useState(15);
   const [isDisabled, setIsDisabled] = useState(true);
   const [isActive, setIsActive] = useState(false);
   const [isStopped, setIsStopped] = useState(false);
 
   const handleStart = () => {
-      setIsDisabled(false);
-      setIsActive(true);
-      
-      const increment = setInterval(() => {
-        setTimer((timer) => timer + 1)
-      }, 1000);
-      if (timer == 15000){
-        handleStop(increment);
-      } 
-      
+    setCount(0);
+    setIsDisabled(false);
+    setIsActive(true);
+    const increment = setInterval(() => {
+      setTimer((timer) => timer - 1);
+    }, 1000);
   }
 
-  const handleStop = (interval: NodeJS.Timer) => {
-    clearInterval(interval);
+  const handleStop = () => {
     setIsDisabled(true);
     setIsActive(false);
     setTimer(0);
   }
 
   useEffect(() => {
-      document.title = `You clicked ${count} times`
+    document.title = `You clicked ${count} times`
+    if(timer <= 0){
+      return () => handleStop();
+    }
   });
 
   return (
