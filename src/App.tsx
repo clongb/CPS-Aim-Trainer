6c89e3d5d67879e2bdf22bf9bb7f4f79f18d15b0
unknown <clongb@udel.edu>
2021-09-24 00:12:42 -0400

    added timer

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,30 @@ import React, { useState, useEffect } from 'react';
 
 function App(): JSX.Element {
   const [count, setCount] = useState(0);
+  const [timer, setTimer] = useState(0);
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [isActive, setIsActive] = useState(false);
+  const [isStopped, setIsStopped] = useState(false);
+
+  const handleStart = () => {
+      setIsDisabled(false);
+      setIsActive(true);
+      
+      const increment = setInterval(() => {
+        setTimer((timer) => timer + 1)
+      }, 1000);
+      if (timer == 15000){
+        handleStop(increment);
+      } 
+      
+  }
+
+  const handleStop = (interval: NodeJS.Timer) => {
+    clearInterval(interval);
+    setIsDisabled(true);
+    setIsActive(false);
+    setTimer(0);
+  }
 
   useEffect(() => {
       document.title = `You clicked ${count} times`
@@ -10,10 +34,14 @@ function App(): JSX.Element {
 
   return (
     <div className="App">
+      <p>{timer}</p>
       <p>You clicked {count} times!</p>
-      <button onClick={() => setCount(count+1)}>
-          Click me
-      </button>
+      <div className="buttons">
+        <button onClick={handleStart}>Start</button>
+        <button disabled={isDisabled} onClick={() => setCount(count+1)}>
+            Click me
+        </button>
+      </div>
     </div>
   );
 }
